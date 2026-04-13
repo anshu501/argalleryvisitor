be0aabed399cacf5672622abba6f9f2d60304076
anita2076gupta-bit <anita2076gupta@gmail.com>
2026-04-13 10:43:31 +0530

    Update StandardVisitor.java

--- StandardVisitor.java
+++ StandardVisitor.java
@@ -3,3 +3,4 @@ private boolean isEligibleForDiscountUpgrade;
     private final int visitLimit;
     private float discountPercent;
 //defining constructor of the class StandardVisitor which will call the attributes from the super class and initialize attributes 
+public StandardVisitor(int visitorId,String fullName,String gender,String contactNumber,String registrationDate,double ticketCost,
