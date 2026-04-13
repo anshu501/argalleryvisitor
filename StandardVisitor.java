9daf830b9edd0dfd52fdf9f0cfa64acef056e068
anita2076gupta-bit <anita2076gupta@gmail.com>
2026-04-13 10:42:57 +0530

    Update StandardVisitor.java

--- StandardVisitor.java
+++ StandardVisitor.java
@@ -2,3 +2,4 @@ public class StandardVisitor extends ArtGalleryVisitor{
 private boolean isEligibleForDiscountUpgrade;
     private final int visitLimit;
     private float discountPercent;
+//defining constructor of the class StandardVisitor which will call the attributes from the super class and initialize attributes 
