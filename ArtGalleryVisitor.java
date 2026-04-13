ac02f400bd7c46d24f0055418b0423a10a12baf7
anshu501 <anshitag732@gmail.com>
2026-04-13 13:55:49 +0530

    Update ArtGalleryVisitor.java

--- ArtGalleryVisitor.java
+++ ArtGalleryVisitor.java
@@ -21,3 +21,29 @@ public abstract class ArtGalleryVisitor{
     protected double discountAmount;
     protected String artworkName;
     protected double artworkPrice;
+
+    //Constructor of the class ArtGalleryVisitor which will initialize the attributes 
+    public ArtGalleryVisitor(int visitorId,String fullName,String gender,String contactNumber,String registrationDate,double ticketCost,
+    String ticketType){
+        //Initialisation of the attributes inside constructor
+        this.visitorId = visitorId;
+        this.fullName = fullName;
+        this.gender = gender;
+        this.contactNumber = contactNumber;
+        this.registrationDate = registrationDate;
+        this.ticketCost = ticketCost;
+        this.ticketType = ticketType;
+
+        this.visitCount = 0;
+        this.rewardPoints = 0;
+        this.cancelCount = 0;
+        this.buyCount = 0;
+        this.discountAmount = 0;
+        this.finalPrice = 0;
+        this.refundableAmount = 0;
+        this.isActive = false;
+        this.isBought = false;
+        this.cancellationReason = "";
+
+    }
+
